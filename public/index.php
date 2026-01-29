c2b8d1422b7b66ba525c76bb958a935e68361481
Banditov <vito20samsung21@gmail.com>
2026-01-29 16:17:32 +0700

    First commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__ . '/../app/core/Router.php';
+
+    use App\Core\Router;
+
+    $router = new Router();
+    $router->run();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
